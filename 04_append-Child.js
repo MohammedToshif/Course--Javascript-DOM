af10a9a31aedfcc7971c09b761fc046d1347126c
MohammedToshif <toshif92612@gmail.com>
2024-06-01 11:37:38 +0530

    DOM - append-Child

---
+++ 04_append-Child.js
@@ -0,0 +1,29 @@
+ 
+//  Parent Element
+
+// const myList = document.getElementById("myList");
+
+
+// New List Item ( <li>Banana</> ) 
+
+// let newListItem = document.createElement('li')
+// newListItem.textContent = 'Banana';
+
+// myList.appendChild(newListItem);
+
+
+            // (02)
+
+// Parent Element            
+
+const myList = document.getElementById("myList");
+
+const fruitList = ["Banana", "Orange", "Mango", "Lemon"];
+
+for (let fruit of fruitList) {
+    let newListItem = document.createElement('li');
+    newListItem.textContent = fruit;
+
+    myList.appendChild(newListItem);
+}
+
