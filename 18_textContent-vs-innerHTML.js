511c6df12088caef440dd491cf927baaebd4a660
MohammedToshif <toshif92612@gmail.com>
2024-06-02 18:17:33 +0530

    JS-DOM - text content vs inner HTML

---
+++ 18_textContent-vs-innerHTML.js
@@ -0,0 +1,10 @@
+
+const myDiv = document.getElementById('my-div');
+
+myDiv.innerHTML = 'Hey my name is <strong>Toshif</strong>'; // bold me toshif print hoga
+
+
+// myDiv.textContent = 'Hey my name is <strong>Toshif</strong>';    // Hey my name is <strong>Toshif</strong>
+
+
+// textContents is all text contained by an element and all its children that are for formatting purposes only. innerText returns all text contained by an element and all its child elements. innerHtml returns all text, including html tags, that is contained by an element.
