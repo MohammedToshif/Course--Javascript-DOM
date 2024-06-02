969c131a23f2b8bb99063bd0ad1c3504f98e1358
MohammedToshif <toshif92612@gmail.com>
2024-06-02 13:56:33 +0530

    JS-DOM - QueryselectorALL

---
+++ 14_query-selector-all.js
@@ -0,0 +1,25 @@
+// Query selector All lets you find a list of element that match the cSS selector
+
+// const boxes = document.querySelectorAll(".box");
+// console.log(boxes);
+
+
+
+// boxes.forEach((e) => {         // e = single box
+//     e.style.color = 'red';
+// });
+
+
+
+        // (02)
+
+
+// const items = document.querySelectorAll("li");
+// console.log(items);
+
+
+
+
+// const list2 = document.querySelector("#list2");
+// const items = list2.querySelectorAll("li");
+// console.log(items);
