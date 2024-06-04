b63008fc42cc5a17824ff6fd81fe0d776b1a29b2
MohammedToshif <toshif92612@gmail.com>
2024-06-04 18:55:32 +0530

    more methods

---
+++ 32_other-method.js
@@ -0,0 +1,36 @@
+
+// closest ()
+/*
+The closest() method of the Element interface traverses the element and its parents (heading toward the document root) until it finds a node that matches the specified 
+
+*/
+
+
+// append ()
+/*
+The Document.append() method inserts a set of Node objects or string objects after the last child of the document. String objects are inserted as equivalent Text nodes.
+
+
+append method to append multiple to an existing element
+*/
+
+
+
+// insertAdjacentHTML()
+/*
+The insertAdjacentHTML() method of the Element interface parses the specified text as HTML or XML and inserts the resulting nodes into the DOM tree at a specified position.
+
+insert some html string relative to another html element
+*/
+
+
+// matches ()
+/*
+The match() method matches a string against a regular expression ** The match() method returns an array with the matches. The match() method returns null if no match is found.
+*/
+
+
+// replaceWith ()
+/*
+replaceWith() method replaces this Element in the children list of its parent with a set of Node or string objects. String objects are inserted as equivalent Text nodes.
+*/
